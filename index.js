4b8daef62450203108eeb1d0db58fc7992bbfb35
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-12-09 17:56:00 -0500

    Minor adjustments to index.js and index.ejs

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 // Load Node modules
 const express = require('express');
 const ejs = require('ejs');
+const cors = require('cors')
+const PORT = process.env.PORT || 3000
 // Initialise Express
 const app = express();
 // const { MongoClient } = require("mongodb");
 // Render static files
-app.use(express.static('public'));
+app.use(express.json())
+app.use(express.static('public'))
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+})
 
 // *** GET Routes - display pages ***
 // Root Route
 app.get('/', function (req, res) {
     res.render('index.ejs');
-    
+
 });
 
-app.get('/Login', function(req, res) {
+app.get('/login', function(req, res) {
     res.render('rgtr.ejs');
 });
-  
+
 app.post("/",(req, res) => {});
 
 app.put("/", (req, res) => {});
 
 app.delete("/", (req, res) => {});
 
-// Port website will run on
-let PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-  console.log(`Server listening on {PORT}`);
-});
+
+
+
+
+//ROUTES
+app.get('/signUpQuiz', (req, res) =>{
+  res.render('signUpQuiz.ejs')
+})
