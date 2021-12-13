d99e5069cdb5b6ddc45482f706cf27de2c0680c6
Davida Garner <davidac.garner@gmail.com>
2021-12-13 15:45:13 -0500

    updated index
    added addl routes to index.js

--- index.js
+++ index.js
@@ -62,3 +62,113 @@ MongoClient.connect(MongoDB_URL, { useUnifiedTopology: true }).then(
     app.delete("/", (req, res) => {});
   }
 );
+
+
+// Davida's input 
+    // QP Posts routes ===============================================================
+    app.get('/post', isLoggedIn, function(req, res) {
+      db.collection('posts').findOne({_id: ObjectId(req.query.id)}, (err, result) => {
+          db.collection('comments').find().toArray((error, rslt) => {
+              if (err) return console.log(err)
+              console.log(result, req.query.id);
+              res.render('post.ejs', {
+                  user : req.user,
+                  post: result,
+                  comments: rslt
+              })
+          })
+      })
+  });
+  var storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+      cb(null, 'public/img/')
+      },
+      filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now() + ".png")
+      }
+  })
+  var upload = multer({storage: storage})
+  app.post('/createPost', upload.single('file-to-upload'), (req, res) => {
+    db.collection('posts').save({
+      image: "img/" + req.file.filename,
+      caption: req.body.caption,
+      user: req.user._id,
+      likes: 0,
+      timestamp: req.body.timestamp
+      }, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/feed')
+    })
+  })
+  app.put('/updatePost', (req, res) => {
+    db.collection('posts').findOneAndUpdate({
+      image: req.body.image,
+      caption: req.body.caption,
+      user: req.user._id,
+      likes: 0
+      },{ $set:{ caption: req.body.newCaption } },{ sort:{_id: -1} }, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database');
+      res.redirect('/feed');
+    })
+  })
+
+  app.delete('/delPost', (req, res) => {
+    db.collection('posts').findOneAndDelete({
+      _id: ObjectId(req.body.id)
+      }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Message deleted!')
+    })
+  })
+
+  //USER'S POSTS
+  app.get('/profile', isLoggedIn, function(req, res) {
+      db.collection('posts').find().toArray((err, result) => {
+          for(post in result) {
+              if(post.id != ObjectId(req.user.id))
+                  delete result.post
+          }
+        if (err) return console.log(err)
+        res.render('profile.ejs', {
+          user : req.user,
+          posts: result
+        })
+      })
+  });
+  // COMMENTS SECTION
+
+  app.get('/comment', isLoggedIn, function(req, res) {
+      db.collection('comments').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('post.ejs', {
+          user : req.user,
+          comments: result
+      })
+      })
+  });
+
+  app.post('/createComment', (req, res) => {
+      const comingFromPage = req.headers['referer'].slice(req.headers['origin'].length);
+      db.collection('comments').insertOne({
+          comment: req.body.comment, 
+          poster: req.user._id, 
+          post: req.body.postId,
+          timestamp: req.body.timestamp
+      }, (err, result) => {
+          if (err) return res.send(err);
+          console.log('Comment Created');
+          res.redirect(comingFromPage);
+      })
+  })
+  app.delete('/delComment', (req, res) => {
+      db.collection('comments').findOneAndDelete({
+        comment: req.body.comment,
+        poster: req.user._id,
+        post: req.body.postId
+        }, (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('Message deleted!')
+      })
+    })
